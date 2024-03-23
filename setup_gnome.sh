fc10cff3b5cae58f874f7b15e89f917fb1802bed
George Andromidas <89165929+GAndromidas@users.noreply.github.com>
2024-03-24 01:41:31 +0200

    setup_gnome.sh

--- setup_gnome.sh
+++ setup_gnome.sh
@@ -11,32 +11,9 @@ echo -e "\033[1;34m"
 echo -e "GNOME-SPECIFIC PROGRAMS INSTALLED SUCCESSFULLY.\n"
 echo -e "\033[0m"
 
-# Configure firewall for GNOME
-echo -e "\033[1;34m"
-echo -e "CONFIGURING FIREWALL FOR GNOME...\n"
-echo -e "\033[0m"
-sudo systemctl enable --now ufw
-
-# Default policies
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-
-# Allow SSH
-sudo ufw allow ssh
-
-# Enable logging
-sudo ufw logging on
-
-# Enable rate limiting to prevent DoS attacks
-sudo ufw limit ssh/tcp
-
-# Enable UFW
-sudo ufw --force enable
-
-echo -e "\033[1;34m"
-echo -e "FIREWALL CONFIGURED SUCCESSFULLY FOR GNOME.\n"
-echo -e "\033[0m"
-
 # Gnome Layout Shift+Alt Fix
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt_L']"
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
+
+# Gnome Enable VRR (Experimental)
+gsettings set org.gnome.mutter experimental-features "['variable-refresh-rate']"
