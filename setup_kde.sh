889809fe12334d86e299131cf3e2235749c15493
George Andromidas <89165929+GAndromidas@users.noreply.github.com>
2024-03-24 01:38:19 +0200

    setup_kde.sh

--- setup_kde.sh
+++ setup_kde.sh
@@ -14,27 +14,10 @@ echo -e "\033[0m"
 echo -e "\033[1;34m"
 echo -e "CONFIGURING FIREWALL FOR KDE...\n"
 echo -e "\033[0m"
-sudo systemctl enable --now ufw
-
-# Default policies
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-
-# Allow SSH
-sudo ufw allow ssh
 
 # Allow specific services for KDE
 sudo ufw allow 1714:1764/tcp
 sudo ufw allow 1714:1764/udp
-
-# Enable logging
-sudo ufw logging on
-
-# Enable rate limiting to prevent DoS attacks
-sudo ufw limit ssh/tcp
-
-# Enable UFW
-sudo ufw --force enable
 
 echo -e "\033[1;34m"
 echo -e "FIREWALL CONFIGURED SUCCESSFULLY FOR KDE.\n"
