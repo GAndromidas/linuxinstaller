db87c48b01c2f82d621d6df5c2f32cfbd1e97364
George Andromidas <89165929+GAndromidas@users.noreply.github.com>
2024-03-24 19:00:08 +0200

    setup_kde.sh

--- setup_kde.sh
+++ setup_kde.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Install KDE-specific programs
-echo -e "\033[1;34m"
-echo -e "INSTALLING KDE-SPECIFIC PROGRAMS...\n"
-echo -e "\033[0m"
-sudo pacman -S --needed --noconfirm ark gwenview kdeconnect kwalletmanager kvantum okular packagekit-qt6 spectacle qbittorrent
-sudo pacman -Rcs --noconfirm htop
-sudo flatpak install -y flathub net.davidotek.pupgui2
-echo -e "\033[1;34m"
-echo -e "KDE-SPECIFIC PROGRAMS INSTALLED SUCCESSFULLY.\n"
-echo -e "\033[0m"
+  echo -e "\033[1;34m"
+  echo -e "INSTALLING KDE-SPECIFIC PROGRAMS...\n"
+  echo -e "\033[0m"
+  sudo pacman -S --needed --noconfirm ark gwenview kdeconnect kwalletmanager kvantum okular packagekit-qt6 spectacle qbittorrent
+  sudo pacman -Rcs --noconfirm htop
+  sudo flatpak install -y flathub net.davidotek.pupgui2
+  echo -e "\033[1;34m"
+  echo -e "KDE-SPECIFIC PROGRAMS INSTALLED SUCCESSFULLY.\n"
+  echo -e "\033[0m"
 
 # Configure firewall for KDE
-echo -e "\033[1;34m"
-echo -e "CONFIGURING FIREWALL FOR KDE...\n"
-echo -e "\033[0m"
+  echo -e "\033[1;34m"
+  echo -e "CONFIGURING FIREWALL FOR KDE...\n"
+  echo -e "\033[0m"
 
 # Allow KDE Connect ports
-sudo ufw allow 1714:1764/tcp
-sudo ufw allow 1714:1764/udp
+  sudo ufw allow 1714:1764/tcp
+  sudo ufw allow 1714:1764/udp
 
-echo -e "\033[1;34m"
-echo -e "FIREWALL CONFIGURED SUCCESSFULLY FOR KDE.\n"
-echo -e "\033[0m"
+  echo -e "\033[1;34m"
+  echo -e "FIREWALL CONFIGURED SUCCESSFULLY FOR KDE.\n"
+  echo -e "\033[0m"
