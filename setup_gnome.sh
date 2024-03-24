20f9f3af61a8b4ea935405c81b01b54695c26543
George Andromidas <89165929+GAndromidas@users.noreply.github.com>
2024-03-24 18:34:13 +0200

    setup_gnome.sh

--- setup_gnome.sh
+++ setup_gnome.sh
@@ -6,7 +6,7 @@ echo -e "INSTALLING GNOME-SPECIFIC PROGRAMS...\n"
 echo -e "\033[0m"
 sudo pacman -S --needed --noconfirm gnome-tweaks gufw seahorse transmission-gtk
 sudo pacman -Rcs --noconfirm epiphany gnome-contacts gnome-maps gnome-music gnome-tour htop snapshot totem
-sudo flatpak install -y flathub com.mattjakeman.ExtensionManager net.davidotek.pupgui2
+sudo flatpak install -y flathub com.mattjakeman.ExtensionManager net.davidotek.pupgui2 org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
 yay -S --needed --noconfirm ocs-url
 echo -e "\033[1;34m"
 echo -e "GNOME-SPECIFIC PROGRAMS INSTALLED SUCCESSFULLY.\n"
@@ -21,3 +21,6 @@ gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<
 
 # Gnome Enable VRR (Experimental)
 gsettings set org.gnome.mutter experimental-features "['variable-refresh-rate']"
+
+# Gnome Enable Adw-GTK3-Dark
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
