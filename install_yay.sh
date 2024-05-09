8366fc31cfcbd41677376e807e3342f3e112b369
George Andromidas <89165929+GAndromidas@users.noreply.github.com>
2024-05-09 18:52:10 +0300

    install_yay.sh

---
+++ install_yay.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Function to install Yay
+install_yay() {
+    log "Installing Yay..."
+    git clone https://aur.archlinux.org/yay.git
+    cd yay || { log "Failed to change directory to yay. Exiting."; exit 1; }
+    makepkg -si --needed --noconfirm || { log "Failed to install Yay. Exiting."; exit 1; }
+    cd .. && rm -rf yay || { log "Failed to clean up Yay files. Exiting."; exit 1; }
+    log "Yay installed successfully."
+}
+
+# Function to install AUR packages using Yay
+install_yay_packages() {
+    log "Installing AUR packages with Yay..."
+    yay_packages=($(cat "$HOME/archinstaller/yay_packages.txt"))
+    if ! yay -S --needed --noconfirm "${yay_packages[@]}"; then
+        log "Failed to install AUR packages with Yay. Exiting."
+        exit 1
+    fi
+    log "AUR packages installed successfully."
+}
+
+# Main execution flow
+main() {
+    install_yay
+    install_yay_packages
+}
+
+main
