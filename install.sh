94859b164e8a304d43208dc87e5e46b19918635e
George Andromidas <karlandromidas@gmail.com>
2024-06-20 20:03:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,35 @@ LOADER_CONF="/boot/loader/loader.conf"
 CONFIGS_DIR="$HOME/archinstaller/configs"
 SCRIPTS_DIR="$HOME/archinstaller/scripts"
 
+# Color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
+# Function to print messages with colors
+print_info() {
+    echo -e "${CYAN}$1${RESET}"
+}
+
+print_success() {
+    echo -e "${GREEN}$1${RESET}"
+}
+
+print_warning() {
+    echo -e "${YELLOW}$1${RESET}"
+}
+
+print_error() {
+    echo -e "${RED}$1${RESET}"
+}
+
 # Function to identify the installed Linux kernel type
 identify_kernel_type() {
-    printf "Identifying installed Linux kernel type...\n"
+    print_info "Identifying installed Linux kernel type..."
     if pacman -Q linux-zen &>/dev/null; then
         KERNEL_HEADERS="linux-zen-headers"
     elif pacman -Q linux-hardened &>/dev/null; then
@@ -22,92 +48,92 @@ identify_kernel_type() {
     elif pacman -Q linux-lts &>/dev/null; then
         KERNEL_HEADERS="linux-lts-headers"
     fi
-    printf "%s kernel headers will be installed.\n" "$KERNEL_HEADERS"
+    print_info "$KERNEL_HEADERS kernel headers will be installed."
 }
 
 # Function to install kernel headers
 install_kernel_headers() {
     identify_kernel_type
-    printf "Installing kernel headers...\n"
+    print_info "Installing kernel headers..."
     if sudo pacman -S --needed --noconfirm "$KERNEL_HEADERS"; then
-        printf "Kernel headers installed successfully.\n"
+        print_success "Kernel headers installed successfully."
     else
-        printf "Error: Failed to install kernel headers.\n"
+        print_error "Error: Failed to install kernel headers."
         exit 1
     fi
 }
 
 # Function to remove Linux kernel fallback image
 remove_kernel_fallback_image() {
-    printf "Removing Linux kernel fallback image...\n"
-    sudo rm /boot/loader/entries/*fallback* && printf "Linux kernel fallback image removed successfully.\n"
+    print_info "Removing Linux kernel fallback image..."
+    sudo rm /boot/loader/entries/*fallback* && print_success "Linux kernel fallback image removed successfully."
 }
 
 # Function to make Systemd-Boot silent
 make_systemd_boot_silent() {
-    printf "Making Systemd-Boot silent...\n"
+    print_info "Making Systemd-Boot silent..."
     linux_entry=$(find "$ENTRIES_DIR" -type f \( -name '*_linux.conf' -o -name '*_linux-zen.conf' \) ! -name '*_linux-fallback.conf' -print -quit)
 
     if [ -z "$linux_entry" ]; then
-        printf "Error: Linux entry not found.\n"
+        print_error "Error: Linux entry not found."
         exit 1
     fi
 
     sudo sed -i '/options/s/$/ quiet loglevel=3 systemd.show_status=auto rd.udev.log_level=3/' "$linux_entry" && \
-    printf "Silent boot options added to Linux entry: %s.\n" "$(basename "$linux_entry")"
+    print_success "Silent boot options added to Linux entry: $(basename "$linux_entry")."
 }
 
 # Function to change loader.conf
 change_loader_conf() {
-    printf "Changing loader.conf...\n"
+    print_info "Changing loader.conf..."
     sudo sed -i 's/^timeout.*/timeout 3/' "$LOADER_CONF"
     sudo sed -i 's/^#console-mode.*/console-mode max/' "$LOADER_CONF"
-    printf "Loader configuration updated.\n"
+    print_success "Loader configuration updated."
 }
 
 # Function to enable asterisks for password in sudoers
 enable_asterisks_sudo() {
-    printf "Enabling asterisks for password input in sudoers...\n"
+    print_info "Enabling asterisks for password input in sudoers..."
     echo "Defaults env_reset,pwfeedback" | sudo EDITOR='tee -a' visudo && \
-    printf "Password feedback enabled in sudoers.\n"
+    print_success "Password feedback enabled in sudoers."
 }
 
 # Function to configure Pacman
 configure_pacman() {
-    printf "Configuring Pacman...\n"
+    print_info "Configuring Pacman..."
     sudo sed -i '
         /^#Color/s/^#//
         /^Color/a ILoveCandy
         /^#VerbosePkgLists/s/^#//
         s/^#ParallelDownloads = 5/ParallelDownloads = 10/
     ' /etc/pacman.conf && \
-    printf "Pacman configuration updated successfully.\n"
+    print_success "Pacman configuration updated successfully."
 }
 
 # Function to update mirrorlist and modify reflector.conf
 update_mirrorlist() {
-    printf "Updating Mirrorlist...\n"
+    print_info "Updating Mirrorlist..."
     sudo pacman -S --needed --noconfirm reflector rsync
 
     sudo sed -i 's/^--latest .*/--latest 10/' /etc/xdg/reflector/reflector.conf
     sudo sed -i 's/^--sort .*/--sort rate/' /etc/xdg/reflector/reflector.conf
-    printf "reflector.conf updated successfully.\n"
+    print_success "reflector.conf updated successfully."
 
     sudo reflector --verbose --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist && \
     sudo pacman -Syyy && \
-    printf "Mirrorlist updated successfully.\n"
+    print_success "Mirrorlist updated successfully."
 }
 
 # Function to update the system
 update_system() {
-    printf "Updating System...\n"
+    print_info "Updating System..."
     sudo pacman -Syyu --noconfirm && \
-    printf "System updated successfully.\n"
+    print_success "System updated successfully."
 }
 
 # Function to install Oh-My-ZSH and ZSH plugins
 install_zsh() {
-    printf "Configuring ZSH...\n"
+    print_info "Configuring ZSH..."
     sudo pacman -S --needed --noconfirm zsh
     yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     sleep 1
@@ -116,89 +142,89 @@ install_zsh() {
     sleep 1
 
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    printf "ZSH configured successfully.\n"
+    print_success "ZSH configured successfully."
 }
 
 # Function to change shell to ZSH
 change_shell_to_zsh() {
-    printf "Changing Shell to ZSH...\n"
+    print_info "Changing Shell to ZSH..."
     sudo chsh -s "$(which zsh)"
     chsh -s "$(which zsh)"
-    printf "Shell changed to ZSH.\n"
+    print_success "Shell changed to ZSH."
 }
 
 # Function to move .zshrc
 move_zshrc() {
-    printf "Copying .zshrc to Home Folder...\n"
+    print_info "Copying .zshrc to Home Folder..."
     mv "$CONFIGS_DIR/.zshrc" "$HOME/" && \
-    printf ".zshrc copied successfully.\n"
+    print_success ".zshrc copied successfully."
 }
 
 # Function to install starship and move starship.toml
 install_starship() {
-    printf "Installing Starship prompt...\n"
+    print_info "Installing Starship prompt..."
     if curl -sS https://starship.rs/install.sh | sh -s -- -y; then
-        printf "Starship prompt installed successfully.\n"
+        print_success "Starship prompt installed successfully."
         mkdir -p "$HOME/.config"
         if [ -f "$CONFIGS_DIR/starship.toml" ]; then
             mv "$CONFIGS_DIR/starship.toml" "$HOME/.config/starship.toml"
-            printf "starship.toml moved to $HOME/.config/\n"
+            print_success "starship.toml moved to $HOME/.config/"
         else
-            printf "starship.toml not found in $CONFIGS_DIR/\n"
+            print_warning "starship.toml not found in $CONFIGS_DIR/"
         fi
     else
-        printf "Starship prompt installation failed.\n"
+        print_error "Starship prompt installation failed."
     fi
 }
 
 # Function to configure locales
 configure_locales() {
-    printf "Configuring Locales...\n"
+    print_info "Configuring Locales..."
     sudo sed -i 's/#el_GR.UTF-8 UTF-8/el_GR.UTF-8 UTF-8/' /etc/locale.gen
     sudo locale-gen && \
-    printf "Locales generated successfully.\n"
+    print_success "Locales generated successfully."
 }
 
 # Function to install YAY
 install_yay() {
-    printf "Installing YAY...\n"
+    print_info "Installing YAY..."
     cd /tmp
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si --noconfirm
     cd ..
     rm -rf yay
-    printf "YAY installed successfully.\n"
+    print_success "YAY installed successfully."
 }
 
 # Function to install programs
 install_programs() {
-    printf "Installing Programs...\n"
+    print_info "Installing Programs..."
     (cd "$SCRIPTS_DIR" && ./install_programs.sh) && \
-    printf "Programs installed successfully.\n"
+    print_success "Programs installed successfully."
 
     install_flatpak_programs
 }
 
 # Function to install flatpak programs
 install_flatpak_programs() {
-    printf "Installing Flatpak Programs...\n"
+    print_info "Installing Flatpak Programs..."
     (cd "$SCRIPTS_DIR" && ./install_flatpak_programs.sh) && \
-    printf "Flatpak programs installed successfully.\n"
+    print_success "Flatpak programs installed successfully."
 
     install_aur_programs
 }
 
 # Function to install AUR programs
 install_aur_programs() {
-    printf "Installing AUR Programs...\n"
+    print_info "Installing AUR Programs..."
     (cd "$SCRIPTS_DIR" && ./install_aur_programs.sh) && \
-    printf "AUR programs installed successfully.\n"
+    print_success "AUR programs installed successfully."
 }
 
 # Function to enable services
 enable_services() {
-    printf "Enabling Services...\n"
+    print_info "Enabling Services..."
     local services=(
         "bluetooth"
         "cronie"
@@ -215,32 +241,32 @@ enable_services() {
     for service in "${services[@]}"; do
         if systemctl list-unit-files | grep -q "^$service"; then
             sudo systemctl enable --now "$service"
-            printf "%s enabled.\n" "$service"
+            print_success "$service enabled."
         else
-            printf "%s is not installed.\n" "$service"
+            print_warning "$service is not installed."
         fi
     done
 
-    printf "Services enabled successfully.\n"
+    print_success "Services enabled successfully."
 }
 
 # Function to create fastfetch config
 create_fastfetch_config() {
-    printf "Creating fastfetch config...\n"
+    print_info "Creating fastfetch config..."
     fastfetch --gen-config && \
-    printf "fastfetch config created successfully.\n"
+    print_success "fastfetch config created successfully."
 
-    printf "Copying fastfetch config from repository to ~/.config/fastfetch/...\n"
+    print_info "Copying fastfetch config from repository to ~/.config/fastfetch/..."
     cp "$CONFIGS_DIR/config.jsonc" "$HOME/.config/fastfetch/config.jsonc" && \
-    printf "fastfetch config copied successfully.\n"
+    print_success "fastfetch config copied successfully."
 }
 
 # Function to configure firewall
 configure_firewall() {
-    printf "Configuring Firewall...\n"
+    print_info "Configuring Firewall..."
 
     if command -v ufw > /dev/null 2>&1; then
-        printf "Using UFW for firewall configuration.\n"
+        print_info "Using UFW for firewall configuration."
         commands=(
             "sudo ufw default deny incoming"
             "sudo ufw default allow outgoing"
@@ -250,7 +276,7 @@ configure_firewall() {
             "sudo ufw --force enable"
         )
     elif command -v firewall-cmd > /dev/null 2>&1; then
-        printf "Using firewalld for firewall configuration.\n"
+        print_info "Using firewalld for firewall configuration."
         commands=(
             "sudo firewall-cmd --permanent --add-service=ssh"
             "sudo firewall-cmd --permanent --add-service=kdeconnect"
@@ -258,7 +284,7 @@ configure_firewall() {
             "sudo firewall-cmd --reload"
         )
     else
-        printf "No compatible firewall found. Please install ufw or firewalld.\n"
+        print_error "No compatible firewall found. Please install ufw or firewalld."
         return 1
     fi
 
@@ -266,30 +292,30 @@ configure_firewall() {
         eval "$cmd"
     done
 
-    printf "Firewall configured successfully.\n"
+    print_success "Firewall configured successfully."
 }
 
 # Function to clear unused packages and cache
 clear_unused_packages_cache() {
-    printf "Clearing Unused Packages and Cache...\n"
+    print_info "Clearing Unused Packages and Cache..."
     sudo pacman -Rns $(pacman -Qdtq) --noconfirm
     sudo pacman -Sc --noconfirm
     yay -Sc --noconfirm
     rm -rf ~/.cache/* && sudo paccache -r
-    printf "Unused packages and cache cleared successfully.\n"
+    print_success "Unused packages and cache cleared successfully."
 }
 
 # Function to delete the archinstaller folder
 delete_archinstaller_folder() {
-    printf "Deleting Archinstaller Folder...\n"
+    print_info "Deleting Archinstaller Folder..."
     sudo rm -rf "$HOME/archinstaller" && \
-    printf "Archinstaller folder deleted successfully.\n"
+    print_success "Archinstaller folder deleted successfully."
 }
 
 # Function to reboot system
 reboot_system() {
-    printf "Rebooting System...\n"
-    printf "Press 'y' to reboot now, or 'n' to cancel.\n"
+    print_info "Rebooting System..."
+    printf "${YELLOW}Press 'y' to reboot now, or 'n' to cancel.\n${RESET}"
 
     read -p "Do you want to reboot now? (y/n): " confirm_reboot
 
@@ -298,34 +324,34 @@ reboot_system() {
     done
 
     if [[ "$confirm_reboot" == "y" ]]; then
-        printf "Rebooting now...\n"
+        print_info "Rebooting now..."
         sudo reboot
     else
-        printf "Reboot canceled. You can reboot manually later by typing 'sudo reboot'.\n"
+        print_warning "Reboot canceled. You can reboot manually later by typing 'sudo reboot'."
     fi
 }
 
 # Function to detect bootloader
 detect_bootloader() {
     if [ -d "/sys/firmware/efi" ] && [ -d "/boot/loader" ]; then
-        printf "systemd-boot detected.\n"
+        print_info "systemd-boot detected."
         return 0
     else
-        printf "GRUB detected or no bootloader detected.\n"
+        print_info "GRUB detected or no bootloader detected."
         return 1
     fi
 }
 
 # Function to install GRUB theme
 install_grub_theme() {
-    printf "Installing GRUB theme...\n"
+    print_info "Installing GRUB theme..."
     cd /tmp
     git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
     cd Top-5-Bootloader-Themes
     sudo ./install.sh
     cd ..
     rm -rf Top-5-Bootloader-Themes
-    printf "GRUB theme installed successfully.\n"
+    print_success "GRUB theme installed successfully."
 }
 
 # Main script
