a7ea17fa2ba5bc0beb21056f7f6002a7ae7c27ee
George Andromidas <karlandromidas@gmail.com>
2024-07-24 04:07:02 +0300

    Updates

--- scripts/install_programs.sh
+++ scripts/install_programs.sh
@@ -15,6 +15,7 @@ RESET='\033[0m'
 # Function to print messages with colors
 print_info() { echo -e "${CYAN}$1${RESET}"; }
 print_success() { echo -e "${GREEN}$1${RESET}"; }
+print_warning() { echo -e "${YELLOW}$1${RESET}"; }
 print_error() { echo -e "${RED}$1${RESET}"; }
 
 # Function to print usage information
@@ -22,9 +23,9 @@ print_usage() {
     echo -e "${CYAN}Usage:${RESET}"
     echo -e "${CYAN}$0 [OPTIONS]${RESET}"
     echo -e "Options:"
-    echo -e "  -d, --default        Install default programs for your system"
-    echo -e "  -m, --minimal        Install minimal set of programs for your system"
-    echo -e "  -h, --help           Show this help message and exit"
+    echo -e "  -d, --default    Install default programs for your system and desktop environment"
+    echo -e "  -m, --minimal    Install minimal programs for your system and desktop environment"
+    echo -e "  -h, --help       Show this help message and exit"
 }
 
 # Function to handle errors
@@ -42,16 +43,19 @@ detect_desktop_environment() {
             print_info "KDE detected."
             specific_install_programs=("${kde_install_programs[@]}")
             specific_remove_programs=("${kde_remove_programs[@]}")
+            flatpak_install_function="install_flatpak_programs_kde"
             ;;
         GNOME)
             print_info "GNOME detected."
             specific_install_programs=("${gnome_install_programs[@]}")
             specific_remove_programs=("${gnome_remove_programs[@]}")
+            flatpak_install_function="install_flatpak_programs_gnome"
             ;;
         *)
             print_error "No KDE or GNOME detected. Skipping DE-specific programs."
             specific_install_programs=()
             specific_remove_programs=()
+            flatpak_install_function=""
             ;;
     esac
 }
@@ -68,12 +72,23 @@ remove_programs() {
     fi
 }
 
-# Function to install programs
-install_programs() {
-    print_info "Installing Programs..."
+# Function to install programs via pacman
+install_pacman_programs() {
+    print_info "Installing Pacman Programs..."
     $PACMAN_CMD "${pacman_programs[@]}" "${essential_programs[@]}" "${specific_install_programs[@]}"
     handle_error "Failed to install programs. Exiting..."
     print_success "Programs installed successfully."
+}
+
+# Function to install Flatpak programs
+install_flatpak_programs() {
+    if [ -n "$flatpak_install_function" ]; then
+        print_info "Installing Flatpak Programs..."
+        $flatpak_install_function
+        handle_error "Failed to install Flatpak programs. Exiting..."
+    else
+        print_info "No Flatpak installation function defined for the detected desktop environment."
+    fi
 }
 
 # Function to install DaVinci Resolve dependencies
@@ -96,17 +111,31 @@ install_davinci_resolve() {
     print_success "DaVinci Resolve installed successfully."
 }
 
-# Function to parse command line arguments
+# Function to check if yay is installed
+check_yay() {
+    if ! command -v yay &> /dev/null; then
+        print_error "Error: yay is not installed. Please install yay and try again."
+        exit 1
+    fi
+}
+
+# Function to install AUR packages
+install_aur_packages() {
+    print_info "Installing AUR Packages..."
+    $AUR_INSTALL_CMD "${yay_programs[@]}"
+    handle_error "Failed to install AUR packages. Exiting..."
+    print_success "AUR Packages installed successfully."
+}
+
+# Function to parse command-line arguments
 parse_args() {
     while [[ "$#" -gt 0 ]]; do
         case "$1" in
             -d|--default)
-                pacman_programs=("${pacman_programs_default[@]}")
-                essential_programs=("${essential_programs_default[@]}")
+                installation_mode="default"
                 ;;
             -m|--minimal)
-                pacman_programs=("${pacman_programs_minimal[@]}")
-                essential_programs=("${essential_programs_minimal[@]}")
+                installation_mode="minimal"
                 ;;
             -h|--help)
                 print_usage
@@ -123,7 +152,6 @@ parse_args() {
 }
 
 # Main script
-
 # Programs to install using pacman (Default option)
 pacman_programs_default=(
     android-tools bleachbit btop bluez-utils cmatrix curl dmidecode dosfstools expac eza fastfetch firefox
@@ -167,8 +195,97 @@ gnome_remove_programs=(
     epiphany gnome-contacts gnome-maps gnome-music gnome-tour htop snapshot totem
 )
 
+# Flatpak programs to install for KDE (Default)
+install_flatpak_programs_kde() {
+    print_info "Installing Flatpak Programs for KDE..."
+    flatpak_packages=(
+        com.spotify.Client
+        com.stremio.Stremio
+        io.github.shiftey.Desktop
+        net.davidotek.pupgui2
+    )
+    for package in "${flatpak_packages[@]}"; do
+        sudo flatpak install -y flathub "$package"
+    done
+    print_success "Flatpak Programs for KDE installed successfully."
+}
+
+# Flatpak programs to install for GNOME (Default)
+install_flatpak_programs_gnome() {
+    print_info "Installing Flatpak Programs for GNOME..."
+    flatpak_packages=(
+        com.mattjakeman.ExtensionManager
+        com.spotify.Client
+        com.stremio.Stremio
+        io.github.shiftey.Desktop
+        com.vysp3r.ProtonPlus
+    )
+    for package in "${flatpak_packages[@]}"; do
+        sudo flatpak install -y flathub "$package"
+    done
+    print_success "Flatpak Programs for GNOME installed successfully."
+}
+
+# Minimal Flatpak programs for KDE
+install_flatpak_minimal_kde() {
+    print_info "Installing Minimal Flatpak Programs for KDE..."
+    flatpak_packages=(
+        com.stremio.Stremio
+    )
+    for package in "${flatpak_packages[@]}"; do
+        sudo flatpak install -y flathub "$package"
+    done
+    print_success "Minimal Flatpak Programs for KDE installed successfully."
+}
+
+# Minimal Flatpak programs for GNOME
+install_flatpak_minimal_gnome() {
+    print_info "Installing Minimal Flatpak Programs for GNOME..."
+    flatpak_packages=(
+        com.mattjakeman.ExtensionManager
+        com.stremio.Stremio
+    )
+    for package in "${flatpak_packages[@]}"; do
+        sudo flatpak install -y flathub "$package"
+    done
+    print_success "Minimal Flatpak Programs for GNOME installed successfully."
+}
+
+# AUR Packages to install (Default option)
+yay_programs_default=(
+    dropbox
+    teamviewer
+    via-bin
+)
+
+# AUR Packages to install (Minimal option)
+yay_programs_minimal=(
+    teamviewer
+)
+
 # Parse command line arguments
 parse_args "$@"
+
+# Check for yay
+check_yay
+
+# Set programs to install based on installation mode
+case "$installation_mode" in
+    "default")
+        pacman_programs=("${pacman_programs_default[@]}")
+        essential_programs=("${essential_programs_default[@]}")
+        yay_programs=("${yay_programs_default[@]}")
+        ;;
+    "minimal")
+        pacman_programs=("${pacman_programs_minimal[@]}")
+        essential_programs=("${essential_programs_minimal[@]}")
+        yay_programs=("${yay_programs_minimal[@]}")
+        ;;
+    *)
+        print_error "Invalid choice. Exiting."
+        exit 1
+        ;;
+esac
 
 # Detect desktop environment
 detect_desktop_environment
@@ -183,5 +300,11 @@ fi
 # Remove specified programs
 remove_programs
 
-# Install specified programs
-install_programs
+# Install specified programs via pacman
+install_pacman_programs
+
+# Install Flatpak programs
+install_flatpak_programs
+
+# Install AUR packages
+install_aur_packages
